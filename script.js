69f81b933fb71ea47f921e88a75b05795981daff
Guilherme Alves <guialvest@gmail.com>
2022-01-30 00:34:34 -0300

    projeto3-driveneats/layout_e_interação

--- script.js
+++ script.js
@@ -1,45 +1,84 @@
-let comidaPronta=false, bebidaPronta=false, sobremesaPronta=false;
+let comidaPronta=false, bebidaPronta=false, sobremesaPronta=false,precoComida=0, precoBebida=0, PrecoSobremesa=0,total=0, nomeCliente="", endereco="";
+
+let mensagem="";
+
+let valoresFinais = document.querySelectorAll(".valoresFinais p");
+let pratosEscolhidos = document.querySelectorAll(".pratosEscolhidos p");
+let conferindo = document.querySelector(".conferirPedido");
 
 //acionando botão
 function acionarBotão(){
     if(comidaPronta==true && bebidaPronta==true && sobremesaPronta==true){
-        alert("escolhido");
+        let botaoConfirmar = document.querySelector(".botaoConfirmar");
+        botaoConfirmar.classList.add("acionarBotao");
+        botaoConfirmar.innerHTML="Fechar pedido";
+        botaoConfirmar.removeAttribute("disabled");
     }
 }
 
 //escolhendo 1 prato
-function escolherComida(prato){
+function escolherComida(comida,preco,nome){
     comidaPronta=true;
-    let todos = document.querySelectorAll(".comida");
-    for(let i=0;i<todos.length;i++){
-        todos[i].classList.remove("escolhido");
+    let todos = document.querySelector(".comida .escolhido");
+    if(todos !== null){
+        todos.classList.remove("escolhido");
     }
-    let pratoEscolhido = document.querySelector(prato);
-    pratoEscolhido.classList.add("escolhido");
+    comida.classList.add("escolhido");
+    precoComida=parseInt(preco);
+    pratosEscolhidos[0].innerHTML=nome;
+    valoresFinais[0].innerHTML=preco+",00";
     acionarBotão();
+    atualizarSacola()
 }
 
 //escolhendo 2 prato
-function escolherBebida(bebida){
+function escolherBebida(bebida,preco,nome){
     bebidaPronta=true;
-    let todos = document.querySelectorAll(".bebida");
-    for(let i=0;i<todos.length;i++){
-        todos[i].classList.remove("escolhido");
+    let todos = document.querySelector(".bebida .escolhido");
+    if(todos !== null){
+        todos.classList.remove("escolhido");
     }
-    let pratoEscolhido = document.querySelector(bebida);
-    pratoEscolhido.classList.add("escolhido");
+    bebida.classList.add("escolhido");
+    precoBebida=parseInt(preco);
+    pratosEscolhidos[1].innerHTML=nome;
+    valoresFinais[1].innerHTML=preco+",00";
     acionarBotão();
+    atualizarSacola()
 }
 
 //escolhendo 3 prato
-function escolherSobremesa(sobremesa){
+function escolherSobremesa(sobremesa,preco,nome){
     sobremesaPronta=true;
-    let todos = document.querySelectorAll(".sobremesa");
-    for(let i=0;i<todos.length;i++){
-        todos[i].classList.remove("escolhido");
+    let todos = document.querySelector(".sobremesa .escolhido");
+    if(todos !== null){
+        todos.classList.remove("escolhido");
     }
-    let pratoEscolhido = document.querySelector(sobremesa);
-    pratoEscolhido.classList.add("escolhido");
+    sobremesa.classList.add("escolhido");
+    PrecoSobremesa=parseInt(preco);
+    pratosEscolhidos[2].innerHTML=nome;
+    valoresFinais[2].innerHTML=preco+",00";
     acionarBotão();
+    atualizarSacola()
+}
+function atualizarSacola(){
+    total=precoComida+precoBebida+PrecoSobremesa;
+    let sacola = document.querySelector(".sacola");
+    sacola.classList.remove("escondido");
+    let valorSacola = sacola.querySelector(".sacola p");
+    valorSacola.innerHTML="R$ "+total+",00";
+    pratosEscolhidos[3].innerHTML="TOTAL";
+    valoresFinais[3].innerHTML="R$ "+total+",00";
+}
+function conferirPedido(){
+    conferindo.classList.remove("escondido");
+    nomeCliente = prompt("Quel é o seu nome ?");
+    endereco = prompt("Qual é o seu endereço ?");
+}
+function cancelar(){
+    conferindo.classList.add("escondido");
+}
+function confirmar(){
+    mensagem="De "+nomeCliente+"\n"+"Eu gostaria de pedir os seguintes produtos: 1x sanduiche: "+pratosEscolhidos[0].innerHTML+", 1x "+pratosEscolhidos[1].innerHTML+" e 1x "+pratosEscolhidos[2].innerHTML+", para o endereço: "+endereco;
+    alert(mensagem);
 }
 
